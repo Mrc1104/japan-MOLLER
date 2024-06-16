9ae6a42e1efbda03e5f11a192cfa3a2c6924385b
Ryan Conaway <mc321015@ohio.edu>
2024-06-16 15:21:44 -0400

    Replaces TI Header ROC bank data to read 'MOCK' (data) in ASCII

--- Analysis/src/Coda3EventDecoder.cc
+++ Analysis/src/Coda3EventDecoder.cc
@@ -17,9 +17,9 @@ std::vector<UInt_t> Coda3EventDecoder::EncodePHYSEventHeader()
 	// Could we make this more dynamic by reversing the TBOBJ::Fill mechanism?
 	header.push_back(0xFF501001);
 	header.push_back(0x0000000b); // word count for Trigger Bank
-	header.push_back(0xFF212001); // 0x001 = # of ROCs (is this an issue if we have multiple rocs?)
+	header.push_back(0xFF212001); // 0x001 = # of ROCs (TODO: support multi-ROC Headers)
+	
 	header.push_back(0x010a0004); 
-
 	// evtnum is held by a 64 bit ... for now we set the upper 32 bits to 0
 	header.push_back(++fEvtNumber );
 	header.push_back(0x0);
@@ -31,8 +31,9 @@ std::vector<UInt_t> Coda3EventDecoder::EncodePHYSEventHeader()
 	header.push_back(0x1850001);
 	header.push_back(0xc0da); // TS# Trigger
 	header.push_back(0x2010002);
-	header.push_back(0xc0da01);  	
-	header.push_back(0xc0da02);	
+	header.push_back(0x4D6F636B); // ASCII for 'MOCK'
+	header.push_back(0x4D6F636B); // ASCII for 'MOCK'
+
 	return header;
 }
 
