86d612b2afc4fbd160bb4bbcda1f00e0b6943b7a
Ryan Conaway <mc321015@ohio.edu>
2024-07-02 12:51:39 -0400

    * Modifies CMakelists to allow for ET compilation. * Brings in necessary files from the Hall A analyzer: 	- FindEt.cmake 	- THaEtClient.h 	- THaEtClient.cxx (.C) * Adds THaEtClientLinkDef.h

---
+++ evio/include/THaEtClientLinkDef.h
@@ -0,0 +1,11 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#ifdef ONLINE_ET
+#pragma link C++ class Decoder::THaEtClient+;
+#endif
+
+#endif
