0b9510938da457594d01750278524b79495118d3
Ryan Conaway <mc321015@ohio.edu>
2024-07-13 21:56:37 -0400

    Adjusts the order in which FindET.cmake searches for the ET lib. It will first search for local built ET libs (${ET}) before searching for a ${CODA} installationg

--- cmake/modules/FindET.cmake
+++ cmake/modules/FindET.cmake
@@ -4,18 +4,7 @@
 
 set(arch ${CMAKE_SYSTEM_NAME}-${CMAKE_SYSTEM_PROCESSOR})
 
-if( DEFINED ENV{CODA} )
-  find_library(ET_LIBRARY et
-    PATHS $ENV{CODA}/${arch}/lib
-	  $ENV{CODA}/${CMAKE_SYSTEM_NAME}/lib
-    DOC "Event Transport (ET) library"
-    )
-  find_path(ET_INCLUDE_DIR
-    NAMES et.h
-    PATHS $ENV{CODA}/common/include
-    DOC "Event Transport (ET) header include directory"
-    )
-elseif( DEFINED ENV{ETINSTALL} )
+if( DEFINED ENV{ET} )
   find_library(ET_LIBRARY et
     PATHS $ENV{ETINSTALL}/${arch}/lib
 	  $ENV{ETINSTALL}/${CMAKE_SYSTEM_NAME}/lib
@@ -26,8 +15,18 @@ elseif( DEFINED ENV{ETINSTALL} )
     PATHS $ENV{ETINSTALL}/common/include
     DOC "Event Transport (ET) header include directory"
     )
+elseif( DEFINED ENV{CODA} )
+  find_library(ET_LIBRARY et
+    PATHS $ENV{CODA}/${arch}/lib
+	  $ENV{CODA}/${CMAKE_SYSTEM_NAME}/lib
+    DOC "Event Transport (ET) library"
+    )
+  find_path(ET_INCLUDE_DIR
+    NAMES et.h
+    PATHS $ENV{CODA}/common/include
+    DOC "Event Transport (ET) header include directory"
+    )
 endif()
-
 if(NOT TARGET EVIO::ET AND ET_LIBRARY AND ET_INCLUDE_DIR)
   add_library(EVIO::ET SHARED IMPORTED)
   set_target_properties(EVIO::ET PROPERTIES
