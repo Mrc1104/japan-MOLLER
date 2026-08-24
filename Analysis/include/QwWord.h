113b9eeba3dda6f2bfdbbcf795957cea1c0a114b
Ryan Conaway <mc321015@ohio.edu>
2026-08-24 17:22:07 -0400

    Revert "Adds binary operations"
    This reverts commit 3c45277e8df73973cacd0eaf27eba037f9166776.

--- Analysis/include/QwWord.h
+++ Analysis/include/QwWord.h
@@ -42,25 +42,7 @@ public:
     TString GetModType() const {return fModuleType;};
 
 
-	QwWord& operator=(QwWord const&) = default;
-	QwWord& operator+=(QwWord const& other) {
-		fValue += other.fValue;
-		return *this;
-	}
-	QwWord& operator-=(QwWord const& other) {
-		fValue -= other.fValue;
-		return *this;
-	}
-	QwWord operator+(QwWord const& other) const{
-		QwWord tmp = *this;
-		tmp.fValue += other.fValue;
-		return tmp;
-	}
-	QwWord operator-(QwWord const& other) const {
-		QwWord tmp = *this;
-		tmp.fValue -= other.fValue;
-		return tmp;
-	}
+
 
 
     void PrintID() const {
